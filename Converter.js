933f9205a75455b60ccb0d99520ebd5b9eec9d95
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-06 00:41:49 +0200

    Program functionality done

--- Converter.js
+++ Converter.js
@@ -1,6 +1,3 @@
-const button = document.getElementById('button');
-
-
 function calculate(){
     let currencyIN = document.getElementById('currencyIN');
     let currencyOUT = document.getElementById('currencyOUT');
@@ -13,13 +10,35 @@ function calculate(){
     currencyOUT = currencyOUT.value;
     number = number.value;
 
+    var handle = document.getElementById('response');
+
     if(number < 1){
         alert('Insert a number');
     }
     else {
         let result = ((number * currencyIN) / currencyOUT).toFixed(2);
-    }
+        alert('Result is: '+ result);
 
+        inText = inText + ' (' + currencyIN + ')';
+        outText = outText + ' (' + currencyOUT + ')';
+
+        $.ajax({
+            type: 'POST',
+            url: 'History.php?Converter',
+            data: {
+                currency_in: inText,
+                currency_out: outText,
+                value_in: number,
+                value_out: result
+            },
+            success: function (data) {
+                handle.innerHTML = 'Response:\n' + data;
+            },
+            error: function (jqXHR) {
+                handle.innerText = 'Error: ' + jqXHR.status;
+            }
+        });
+    }
 }
     
 
--- History.php
+++ History.php
@@ -8,15 +8,26 @@ class History extends Currencies
         $stmt = $this->pdo->query("SELECT * FROM history ORDER BY id DESC LIMIT 10");
 
         while ($row = $stmt->fetch()) {
-            echo $row['date'] . "<br />\n";
-            echo $row['currency_in'] . "-" . $row['currency_out'] . "<br />\n";
+            echo "<br />". $row['date'] ."<br />";
+            echo $row['value_in'] ." ". $row['currency_in']  ." - ". $row['value_out'] ." ". $row['currency_out'] ."<br />";
         }
     }
 
-    // function Converter()
-    // {
-    //     $sqlInsert = "INSERT INTO currencies (name, currency_code, exchange_rate, date) VALUES (:name, :code, :rate, :date)";
-    //     $insert = $this->pdo->prepare($sqlInsert);
-    //     $insert->execute(array(":name" => $name, ":code" => $code, ":rate" => $rate, ":date" => $date));  
-    // }
+    function Converter()
+    {
+        $currency_in = $_POST['currency_in'];
+        $currency_out = $_POST['currency_out'];
+        $value_in = $_POST['value_in'];
+        $value_out = $_POST['value_out'];
+        $date = date("Y-m-d");
+
+        $sqlInsert = "INSERT INTO history (currency_in, currency_out, date, value_in, value_out) VALUES (:currency_in, :currency_out, :date, :value_in, :value_out)";
+        $insert = $this->pdo->prepare($sqlInsert);
+        $insert->execute(array(":currency_in" => $currency_in, ":currency_out" => $currency_out, ":date" => $date, ":value_in" => $value_in, ":value_out" => $value_out));
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $History = new History();
+    $History->Converter();
 }
